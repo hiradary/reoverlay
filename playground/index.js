61a3b9c1f89d192ee35e126a6afeb4c3366b93f5
hiradary <hiradwork@gmail.com>
2020-07-09 19:15:24 +0430

    😎 Add more examples in playground

--- playground/index.js
+++ playground/index.js
@@ -21,13 +21,58 @@ const ANIMATION_TYPES = [
   'slideRight',
 ]
 
+const MyModal3 = ({ title, animationType }) => {
+  return (
+    <ModalWrapper
+      contentContainerClassName="playground__modalContentContainer"
+      onClose={() => Reoverlay.hideModal()}
+      animation={animationType}
+    >
+      <h1>{title}</h1>
+      <button onClick={() => Reoverlay.hideAll()}>Hide all</button>
+      <button onClick={() => Reoverlay.hideModal()}>Hide this</button>
+      <button onClick={() => Reoverlay.hideModal('MyModal2')}>Hide second modal</button>
+    </ModalWrapper>
+  )
+}
+
+const MyModal2 = ({ animationType, title }) => {
+  const openThirdModal = () => {
+    Reoverlay.showModal('MyModal3', {
+      animationType,
+      title: 'Third modal!!',
+    })
+  }
+  return (
+    <ModalWrapper
+      contentContainerClassName="playground__modalContentContainer"
+      onClose={() => Reoverlay.hideModal()}
+      animation={animationType}
+    >
+      <h1>{title}</h1>
+      <button onClick={openThirdModal}>Open third modal</button>
+      <button onClick={() => Reoverlay.hideAll()}>Hide all</button>
+      <button onClick={() => Reoverlay.hideModal()}>Hide this</button>
+    </ModalWrapper>
+  )
+}
+
 const MyModal = ({ animationType, title }) => {
+  const openSecondModal = () => {
+    Reoverlay.showModal('MyModal2', {
+      animationType,
+      title: 'Second modal!',
+    })
+  }
   return (
     <ModalWrapper
       animation={animationType}
       contentContainerClassName="playground__modalContentContainer"
+      onClose={() => Reoverlay.hideModal()}
     >
       <h1>{title}</h1>
+      <button onClick={() => Reoverlay.hideModal()}>Hide this</button>
+      <button onClick={openSecondModal}>Open second modal</button>
     </ModalWrapper>
   )
 }
@@ -36,6 +81,14 @@ Reoverlay.config([
   {
     name: 'MyModal',
     component: MyModal,
+  },
+  {
+    name: 'MyModal2',
+    component: MyModal2,
+  },
+  {
+    name: 'MyModal3',
+    component: MyModal3,
   },
 ])
 
