ac23b4d4aa1a9116590a8fb53acb226b42109e6a
hiradary <hiradwork@gmail.com>
2020-07-09 19:39:47 +0430

    🤪 Remove eslint ignore comments

--- playground/index.js
+++ playground/index.js
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
