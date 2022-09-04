37833ab773878473af0379d495b35b6d815ff3a4
Hirad Arshadi <hiradwork@gmail.com>
2022-09-03 19:35:43 -0700

    Updates

---
+++ component---src-pages-404-js-c23ba95bdee2f6980e9a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklanding=self.webpackChunklanding||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),u=n(8405);t.default=function(){return l.createElement(l.Fragment,null,l.createElement(u.HJ,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c23ba95bdee2f6980e9a.js.map
